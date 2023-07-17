524b8903f65e8117ccfce8ac3f8d689fbca321e7
Gabriel Andreoni <gabriel.andreoni@hotmail.com>
2023-07-17 18:50:24 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import Link from 'next/link'
 import './styles/globals.css'
 import { Roboto } from 'next/font/google'
@@ -8,9 +9,9 @@ const roboto = Roboto({ subsets: ['latin'], weight: '300' })
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br" style={{scrollBehavior: 'smooth'}}>
-      <head>
-        <Link rel="icon" href='/public/fav-ico.png' sizes='any' />
-      </head>
+      <Head>
+        <Link rel='icon' href="/public/fav-ico.png" />
+      </Head>
       <body className={roboto.className}>{children}</body>
     </html>
   )
