423837cb620df49fd3fb6a018708811e8e5395b7
Gabriel Andreoni <gabriel.andreoni@hotmail.com>
2023-07-17 18:56:54 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,20 @@
-import Head from 'next/head'
-import Link from 'next/link'
 import './styles/globals.css'
 import { Roboto } from 'next/font/google'
 
 
 const roboto = Roboto({ subsets: ['latin'], weight: '300' })
 
+export const metadata = {
+  title: 'Gabriel Androeoni',
+  description: 'Desenvolvedor Front End',
+  icons: {
+    icon: '/fav-ico.png',
+  },
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br" style={{scrollBehavior: 'smooth'}}>
-      <Head>
-        <Link rel='icon' href="/public/fav-ico.png" />
-      </Head>
       <body className={roboto.className}>{children}</body>
     </html>
   )
