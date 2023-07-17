21fbe69c9f7607bd303544feb52dbf1f3eb89e77
Gabriel Andreoni <gabriel.andreoni@hotmail.com>
2023-07-17 19:21:43 -0300

    add favicon

--- app/layout.js
+++ app/layout.js
@@ -5,16 +5,16 @@ import { Roboto } from 'next/font/google'
 const roboto = Roboto({ subsets: ['latin'], weight: '300' })
 
 export const metadata = {
-  title: 'Gabriel Androeoni',
+  title: 'Gabriel Andreoni',
   description: 'Desenvolvedor Front End',
   icons: {
-    icon: '/favicon.png',
+    icon: '/favicon.ico',
   },
 }
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="pt-br" style={{scrollBehavior: 'smooth'}}>
+    <html lang="pt-br" style={{ scrollBehavior: 'smooth' }}>
       <body className={roboto.className}>{children}</body>
     </html>
   )
